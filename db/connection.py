a62c3cfcebe8b2db272228aa49ba114f45f2bdc5
Jeremy Summers <jeremysummers27@gmail.com>
2025-12-03 22:17:33 -0700

    Finish data insertion functions

--- db/connection.py
+++ db/connection.py
@@ -1,12 +1,19 @@
 from typing import Any
+from datetime import date, datetime
 
 import psycopg2
 from psycopg2 import extras
+from psycopg2.extensions import connection
+from pgvector.psycopg2 import register_vector
+
 import json
 import hashlib
 
 
-def get_connection(dbname: str, user: str, password: str, host: str, port: int) -> psycopg2.extensions.connection:
+EMBEDDING_DIM: int = 1024
+
+
+def get_connection(dbname: str, user: str, password: str, host: str, port: int) -> connection:
     conn = psycopg2.connect(
         dbname=dbname,
         user=user,
@@ -14,6 +21,8 @@ def get_connection(dbname: str, user: str, password: str, host: str, port: int) 
         host=host,
         port=port
     )
+
+    register_vector(conn)
 
     return conn
 
@@ -26,7 +35,7 @@ def compute_state_hash(state_dict: dict[str, dict[str, Any]]) -> int:
     return hash
 
 
-def insert_state(conn: psycopg2.extensions.connection, state_dict: dict[str, dict[str, Any]]) -> int:
+def insert_battle(conn: connection, state_dict: dict[str, dict[str, Any]]) -> int:
     state_hash: int = compute_state_hash(state_dict)
     state_jsonb: extras.Json = extras.Json(state_dict)
 
@@ -52,7 +61,7 @@ def insert_state(conn: psycopg2.extensions.connection, state_dict: dict[str, dic
     return state_id
 
 
-def insert_transition(conn: psycopg2.extensions.connection, step_index: int, episode_id: int, state_id: int, new_state_id: int, action: int, reward: float, terminal: bool) -> int:
+def insert_transition(conn: connection, step_index: int, episode_id: int, state_id: int, new_state_id: int, action: int, reward: float, terminal: bool) -> int:
     with conn.cursor() as cur:
         cur.execute(
             """
@@ -83,3 +92,78 @@ def insert_transition(conn: psycopg2.extensions.connection, step_index: int, epi
         (transition_id,) = cur.fetchone()
     
     return transition_id
+
+
+def insert_episode(conn: connection, version_id: int, start_time: datetime, end_time: datetime | None) -> int:
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO episode (
+                version_id,
+                start_time,
+                end_time
+            )
+            VALUES (%s, %s, %s)
+
+            RETURNING episode_id
+            """,
+            (
+                version_id,
+                start_time,
+                end_time
+            )
+        )
+
+        (episode_id,) = cur.fetchone()
+    
+    return episode_id
+
+
+def insert_model(conn: connection, date_created: date, checkpoint_path: str, metrics: str) -> int:
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO model_version (
+                date_created,
+                checkpoint_path,
+                metrics
+            )
+            VALUES (%s, %s, %s)
+
+            RETURNING version_id
+            """,
+            (
+                date_created,
+                checkpoint_path,
+                metrics
+            )
+        )
+
+        (version_id,) = cur.fetchone()
+    
+    return version_id
+
+
+def insert_battle_embedding(conn: connection, state_id: int, embedding: list[float]) -> int:
+    if len(embedding) != EMBEDDING_DIM:
+        return -1
+
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO battle_state_embedding (
+                embedding
+            )
+            VALUES (%s)
+
+            RETURNING embedding_id
+            """,
+            (
+                state_id,
+                embedding
+            )
+        )
+
+        (embedding_id,) = cur.fetchone()
+    
+    return embedding_id
--- reports/log.md
+++ reports/log.md
@@ -5,4 +5,6 @@
 | 11/29/2025 | Create database schema | 3
 | 11/30/2025 | Set up poke-env | 3
 | 12/1/2025 | Start creating battle state | 2
-| **TOTAL** | | **10** |
+| 12/2/2025 | Implement functions for inserting data | 2
+| 12/3/2025 | Finish data insertion functions; learn about PPO | 3
+| **TOTAL** | | **15** |
